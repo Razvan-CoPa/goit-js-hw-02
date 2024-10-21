86f6dece6b4ece081ec31cc8bbf64840245b7090
Razvan-CoPa <rzparaschiva@gmail.com>
2024-10-21 21:30:26 +0100

    Final

--- task-1.js
+++ task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits){
     if (customerCredits >= (quantity * pricePerDroid)){
-        return "You ordered " + quantity + " droids worth " + (quantity*pricePerDroid) + "credits";
+        return "You ordered " + quantity + " droids worth " + (quantity*pricePerDroid) + " credits.";
     }
     else{
         return ("Insufficient funds!");
